80362a4a50305bae406bee44ca8c08062230c856
sanskriti sharma <sans19.sharma@gmail.com>
2022-01-23 23:12:43 +0530

    day3 completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days=90;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
+
+    
+    
